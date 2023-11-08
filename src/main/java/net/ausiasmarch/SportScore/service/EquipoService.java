e67c142e13026a64a8da4de5ad26760bd122d0d1
AlanMcLure <alanmclurealarcon02@gmail.com>
2023-11-08 13:04:42 +0100

    EquipoService cambiado

--- src/main/java/net/ausiasmarch/SportScore/service/EquipoService.java
+++ src/main/java/net/ausiasmarch/SportScore/service/EquipoService.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import jakarta.servlet.http.HttpServletRequest;
 import net.ausiasmarch.SportScore.entity.EquipoEntity;
+import net.ausiasmarch.SportScore.entity.JugadorEntity;
 import net.ausiasmarch.SportScore.exception.ResourceNotFoundException;
 //import net.ausiasmarch.SportScore.helper.DataGenerationHelper;
 import net.ausiasmarch.SportScore.repository.EquipoRepository;
+import net.ausiasmarch.SportScore.repository.JugadorRepository;
 
 @Service
 public class EquipoService {
@@ -21,7 +23,19 @@ public class EquipoService {
     @Autowired
     HttpServletRequest oHttpServletRequest;
 
+    @Autowired
+    JugadorRepository oJugadorRepository;
+
+    @Autowired
+    JugadorService oJugadorService;
+
     public EquipoEntity get(Long id) {
         return oEquipoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
+
+    public Page<EquipoEntity> getPage(Pageable oPageable) {
+        return oEquipoRepository.findAll(oPageable);
+    }
+
+    
 }
